8d5c43870fec0e2c4166a9b9517787a9808e97ae
Tanishq Kancharla <tanishqkancharla3@gmail.com>
2026-03-05 18:17:49 -0800

    Improve selection mentions and file reference chips (#43)
    * fix: unify file chip styles and normalize mentions
    
    * feat: improve selection mentions and file reference chips
    
    * chore: bump extension version to 0.4.2

--- src/webview/components/MessageItem.tsx
+++ src/webview/components/MessageItem.tsx
@@ -6,6 +6,7 @@ import { isRenderablePart } from "./MessagePartRenderer";
 import { Streamdown } from "../lib/streamdown";
 import { vscode } from "../utils/vscode";
 import { useSync } from "../state/sync";
+import { messageMarkdownComponents } from "./markdownComponents";
 
 interface MessageItemProps {
   message: Message;
@@ -101,7 +102,7 @@ export function MessageItem(props: MessageItemProps) {
                   {(attachment) => (
                     <button
                       type="button"
-                      class="message-attachment"
+                      class="file-chip message-attachment"
                       title={attachment.title ?? attachment.label}
                       onClick={(event) => {
                         event.stopPropagation();
@@ -132,7 +133,11 @@ export function MessageItem(props: MessageItemProps) {
               when={hasRenderableParts()} 
               fallback={
                 <Show when={props.message.text}>
-                  <Streamdown mode={props.isStreaming ? "streaming" : "static"} class="message-text">
+                  <Streamdown
+                    mode={props.isStreaming ? "streaming" : "static"}
+                    components={messageMarkdownComponents}
+                    class="message-text"
+                  >
                     {props.message.text!}
                   </Streamdown>
                 </Show>
